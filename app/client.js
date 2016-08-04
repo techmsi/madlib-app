4853a78747be1426c5d793924e71c142f8e1ebce
techmsi <Sophie.Idromenos@nymag.com>
2016-08-04 12:10:37 -0400

    Client: Entry

---
+++ app/client.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+import Story from './components/Story';
+
+const appContainer = document.getElementById('react-container');
+
+render(<Story />, appContainer);
